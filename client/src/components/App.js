0d506631ee5e14299f3ac8c5fef3d90a28998ee1
John Carbajal <johnxjuanito@gmail.com>
2019-12-02 14:16:40 -0800

    outline

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,64 @@
 import React from "react"
-import { useUsers } from "../hooks"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import Login from "./Login"
+import Sidebar from "./Sidebar"
+import Admin from "./Admin"
+import Will from "./Will"
+import BasicInfo from "./BasicInfo"
+import Assets from "./Assets"
+import Beni from "./Beni"
+import CareTaker from "./CareTaker"
+import Gifts from "./Gifts"
+import Thoughts from "./Thoughts"
 
-function App() {
-  const { users } = useUsers()
-
-  console.log(users)
-
+function App(props) {
   return (
-    <div>
-      {users.map(user => (
-        <p key={user.id}>{user.name}</p>
-      ))}
-    </div>
+    <Router>
+      <div>
+        <ul className="Nav">
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/Sidebar">Sidebar</Link>
+          </li>
+          <li>
+            <Link to="/Admin">Admin</Link>
+          </li>
+          <li>
+            <Link to="/Will">Will</Link>
+          </li>
+          <li>
+            <Link to="/BasicInfo">BasicInfo</Link>
+          </li>
+          <li>
+            <Link to="/Assets">Assets</Link>
+          </li>
+          <li>
+            <Link to="/Beni">Beni</Link>
+          </li>
+          <li>
+            <Link to="/CareTaker">CareTaker</Link>
+          </li>
+          <li>
+            <Link to="/Gifts">Gifts</Link>
+          </li>
+          <li>
+            <Link to="/Thoughts">Thoughts</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/Sidebar" component={Sidebar} />
+        <Route path="/Admin" component={Admin} />
+        <Route path="/Will" component={Will} />
+        <Route path="/BasicInfo" component={BasicInfo} />
+        <Route path="/Assets" component={Assets} />
+        <Route path="/Beni" component={Beni} />
+        <Route path="/CareTaker" component={CareTaker} />
+        <Route path="/Gifts" component={Gifts} />
+        <Route path="/Thoughts" component={Thoughts} />
+      </div>
+    </Router>
   )
 }
 
