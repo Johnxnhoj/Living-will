5e082a717fdd43818e82c8cd119ec9f88fb0fc89
Shey Laroza <smlaroza95@gmail.com>
2019-12-04 14:13:30 -0800

    gifts and thoughts in progress

--- client/src/components/Gifts.js
+++ client/src/components/Gifts.js
@@ -1,4 +1,172 @@
 import React from "react"
-export default props => {
-  return <div>Gifts</div>
+export default (props) => {
+  return (
+    <div>
+      <h1>Gifts</h1>
+      <p>
+        Specify items, such as heirlooms, jewelry or cash amounts that you want
+        to leave to someone.
+      </p>
+      <p>Gift #1</p>
+      <label>I want to give my...</label>
+      <input type="text"></input>
+      <label>To... (Full Name)</label>
+      <input type="text"></input>
+      <div>
+        <label>Relationship To This Person</label>
+        <select>
+          <option value="relation">Select</option>
+          <option value="relation">//Immediate Family//</option>
+          <option value="relation">Spouse</option>
+          <option value="relation">Daughter</option>
+          <option value="relation">Son</option>
+          <option value="relation">Mother</option>
+          <option value="relation">Father</option>
+          <option value="relation">Sister</option>
+          <option value="relation">Brother</option>
+          <option value="relation">Domestic Partner</option>
+          <option value="relation">//Extended Family//</option>
+          <option value="relation">Granddaughter</option>
+          <option value="relation">Grandson</option>
+          <option value="relation">Grandmother</option>
+          <option value="relation">Grandfather</option>
+          <option value="relation">Aunt</option>
+          <option value="relation">Uncle</option>
+          <option value="relation">Niece</option>
+          <option value="relation">Nephew</option>
+          <option value="relation">Cousin</option>
+          <option value="relation">//In-Law Family//</option>
+          <option value="relation">Mother-In-Law</option>
+          <option value="relation">Father-In-Law</option>
+          <option value="relation">Daughter-In-Law</option>
+          <option value="relation">Son-In-Law</option>
+          <option value="relation">Sister-In-Law</option>
+          <option value="relation">Brother-In-Law</option>
+          <option value="relation">//Step Family//</option>
+          <option value="relation">Stepmother</option>
+          <option value="relation">Stepfather</option>
+          <option value="relation">Stepsister</option>
+          <option value="relation">Stepbrother</option>
+          <option value="relation">Stepdaughter</option>
+          <option value="relation">Stepson</option>
+          <option value="relation">//Other//</option>
+          <option value="relation">Friend</option>
+          <option value="relation">Business Partner</option>
+          <option value="relation">Charity</option>
+          <option value="relation">Church</option>
+          <option value="relation">School</option>
+        </select>
+        <label>Alternate Recipient (Full Name)</label>
+        <input type="text"></input>
+
+        <p>Gift #2</p>
+        <label>I want to give my...</label>
+        <input type="text"></input>
+        <label>To... (Full Name)</label>
+        <input type="text"></input>
+        <div>
+          <label>Relationship To This Person</label>
+          <select>
+            <option value="relation">Select</option>
+            <option value="relation">//Immediate Family//</option>
+            <option value="relation">Spouse</option>
+            <option value="relation">Daughter</option>
+            <option value="relation">Son</option>
+            <option value="relation">Mother</option>
+            <option value="relation">Father</option>
+            <option value="relation">Sister</option>
+            <option value="relation">Brother</option>
+            <option value="relation">Domestic Partner</option>
+            <option value="relation">//Extended Family//</option>
+            <option value="relation">Granddaughter</option>
+            <option value="relation">Grandson</option>
+            <option value="relation">Grandmother</option>
+            <option value="relation">Grandfather</option>
+            <option value="relation">Aunt</option>
+            <option value="relation">Uncle</option>
+            <option value="relation">Niece</option>
+            <option value="relation">Nephew</option>
+            <option value="relation">Cousin</option>
+            <option value="relation">//In-Law Family//</option>
+            <option value="relation">Mother-In-Law</option>
+            <option value="relation">Father-In-Law</option>
+            <option value="relation">Daughter-In-Law</option>
+            <option value="relation">Son-In-Law</option>
+            <option value="relation">Sister-In-Law</option>
+            <option value="relation">Brother-In-Law</option>
+            <option value="relation">//Step Family//</option>
+            <option value="relation">Stepmother</option>
+            <option value="relation">Stepfather</option>
+            <option value="relation">Stepsister</option>
+            <option value="relation">Stepbrother</option>
+            <option value="relation">Stepdaughter</option>
+            <option value="relation">Stepson</option>
+            <option value="relation">//Other//</option>
+            <option value="relation">Friend</option>
+            <option value="relation">Business Partner</option>
+            <option value="relation">Charity</option>
+            <option value="relation">Church</option>
+            <option value="relation">School</option>
+          </select>
+          <label>Alternate Recipient (Full Name)</label>
+          <input type="text"></input>
+
+          <p>Gift #3</p>
+          <label>I want to give my...</label>
+          <input type="text"></input>
+          <label>To... (Full Name)</label>
+          <input type="text"></input>
+          <div>
+            <label>Relationship To This Person</label>
+            <select>
+              <option value="relation">Select</option>
+              <option value="relation">//Immediate Family//</option>
+              <option value="relation">Spouse</option>
+              <option value="relation">Daughter</option>
+              <option value="relation">Son</option>
+              <option value="relation">Mother</option>
+              <option value="relation">Father</option>
+              <option value="relation">Sister</option>
+              <option value="relation">Brother</option>
+              <option value="relation">Domestic Partner</option>
+              <option value="relation">//Extended Family//</option>
+              <option value="relation">Granddaughter</option>
+              <option value="relation">Grandson</option>
+              <option value="relation">Grandmother</option>
+              <option value="relation">Grandfather</option>
+              <option value="relation">Aunt</option>
+              <option value="relation">Uncle</option>
+              <option value="relation">Niece</option>
+              <option value="relation">Nephew</option>
+              <option value="relation">Cousin</option>
+              <option value="relation">//In-Law Family//</option>
+              <option value="relation">Mother-In-Law</option>
+              <option value="relation">Father-In-Law</option>
+              <option value="relation">Daughter-In-Law</option>
+              <option value="relation">Son-In-Law</option>
+              <option value="relation">Sister-In-Law</option>
+              <option value="relation">Brother-In-Law</option>
+              <option value="relation">//Step Family//</option>
+              <option value="relation">Stepmother</option>
+              <option value="relation">Stepfather</option>
+              <option value="relation">Stepsister</option>
+              <option value="relation">Stepbrother</option>
+              <option value="relation">Stepdaughter</option>
+              <option value="relation">Stepson</option>
+              <option value="relation">//Other//</option>
+              <option value="relation">Friend</option>
+              <option value="relation">Business Partner</option>
+              <option value="relation">Charity</option>
+              <option value="relation">Church</option>
+              <option value="relation">School</option>
+            </select>
+            <label>Alternate Recipient (Full Name)</label>
+            <input type="text"></input>
+
+            <button>Save And Continue</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- client/src/components/Thoughts.js
+++ client/src/components/Thoughts.js
@@ -1,4 +1,13 @@
 import React from "react"
-export default props => {
-  return <div>Thoughts</div>
+export default (props) => {
+  return (
+    <div>
+      <h1>Thoughts</h1>
+      <p>Jot your personal thoughts or messages here.</p>
+      <textarea rows="10" cols="50"></textarea>
+      <div>
+        <button>Save And Continue</button>
+      </div>
+    </div>
+  )
 }
