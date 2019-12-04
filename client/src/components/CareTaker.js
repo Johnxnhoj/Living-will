8c9a92186c0fe63324619c09adf4c5ab914920b3
Denia Camacho <deniacamacho04@gmail.com>
2019-12-03 18:53:34 -0800

    guardian started

--- client/src/components/CareTaker.js
+++ client/src/components/CareTaker.js
@@ -1,4 +1,21 @@
 import React from "react"
 export default props => {
-  return <div>Care Taker</div>
+  return (
+    <div>
+      <div>Guardian</div>
+      <div>
+        Child Full Name <input type="text"></input>
+      </div>
+      <div>
+        {" "}
+        Guardian Full Name Full Name <input type="text"></input>
+      </div>
+      <div>
+        Alternate Guardian Full Name<input type="text"></input>
+      </div>
+      <div>
+        Extra Alternate Guardian Full Name<input type="text"></input>
+      </div>
+    </div>
+  )
 }
