e3408f571291da943bb11fe2681cc67bf66f33f4
John Carbajal <johnxjuanito@gmail.com>
2019-12-03 18:52:55 -0800

    assests

--- client/src/components/Assets.js
+++ client/src/components/Assets.js
@@ -1,4 +1,33 @@
 import React from "react"
 export default props => {
-  return <div>Assets</div>
+  return (
+    <div className="container-1">
+      <div className="box-1">
+        <div className="box-Person">
+          <div className="Input-1">
+            <p>Name of Beneficiary</p>
+            <input type="name" placeholder="Name of Beneficiary"></input>
+          </div>
+          <div className="Input-1">
+            <p>Beneficiary Address</p>
+            <input type="name" placeholder="Beneficiary Adress"></input>
+          </div>
+          <div className="Input-1">
+            <p>Relation</p>
+            <input type="name" placeholder="Relation"></input>
+          </div>
+          <div className="Input-1">
+            <p>Property</p>
+            <input type="name" placeholder="Property"></input>
+          </div>
+        </div>
+        <div className="box-Person">
+          <div className="Input-1">
+            <p>Type of Estate</p>
+            <input type="name" placeholder="Type of Estate"></input>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import Icon from "../lib/Icon"
-import "../styles/Sidebar.css"
 
 ///importing useSide function from hooks///
 // import { useSide } from "../hooks"
@@ -13,30 +12,28 @@ export default props => {
     /// className="Side" is what holds everything within the side bar///
     /// className="Window" is each Link within the side bar ///
     <div className="Side">
-      <div className="toggle">
-        <Icon icon="bars"></Icon>
+      <Icon icon="times"></Icon>
 
-        <div className="window">
-          <Link to="/Will">Will</Link>
-        </div>
-        <div className="window">
-          <Link to="/Documents">Documents</Link>
-        </div>
-        <div className="window">
-          <Link to="/Executor">Executor</Link>
-        </div>
-        <div className="window">
-          <Link to="/Witness">Witness</Link>
-        </div>
-        <div className="window">
-          <Link to="/Notifications">Notifications</Link>
-        </div>
-        <div className="window">
-          <Link to="/Help">Help</Link>
-        </div>
-        <div className="window">
-          <Link to="/Settings">Settings</Link>
-        </div>
+      <div className="window">
+        <Link to="/Will">Will</Link>
+      </div>
+      <div className="window">
+        <Link to="/Documents">Documents</Link>
+      </div>
+      <div className="window">
+        <Link to="/Executor">Executor</Link>
+      </div>
+      <div className="window">
+        <Link to="/Witness">Witness</Link>
+      </div>
+      <div className="window">
+        <Link to="/Notifications">Notifications</Link>
+      </div>
+      <div className="window">
+        <Link to="/Help">Help</Link>
+      </div>
+      <div className="window">
+        <Link to="/Settings">Settings</Link>
       </div>
     </div>
   )
