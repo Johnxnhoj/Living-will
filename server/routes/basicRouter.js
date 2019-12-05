978b3296500eb2dc11b2afc6559aa95d3256aa23
Denia Camacho <deniacamacho04@gmail.com>
2019-12-05 11:09:18 -0800

    not done basic info backend fn

--- server/routes/basicRouter.js
+++ server/routes/basicRouter.js
@@ -1,13 +1,39 @@
 const router = require("express").Router()
 const db = require("../db")
 
-const users = [
-  { id: 1, name: "Mike" },
-  { id: 2, name: "Ryan" }
-]
+router.post("/user_info", (req, res, next) => {
+  const FullName = req.body.FullName
+  const City = req.body.City
+  const County = req.body.County
+  const State = req.body.State
+  const Email = req.body.Email
 
-router.get("/", (req, res, next) => {
-  res.json(users)
+  const getsql = `INSERT INTO user_info (FullName, City, County, State,Email) VALUES (?,?,?,?,?) `
+
+  db.query(
+    sql,
+    [FullName, City, County, State, Email],
+    (err, results, fields) => {
+      if (err) {
+        throw new Error(err)
+      } else {
+        res.json({
+          message: "Completed Basic Info",
+          results
+        })
+      }
+    }
+  )
 })
+
+// router.post("/will", (req,res,next)=>{
+//   const FullName = req.body.FullName
+//   const City = req.body.City
+//   const County = req.body.County
+//   const State = req.body.State
+//   const Email = req.body.Email
+
+//   db.query("SELECT FullName")
+// })
 
 module.exports = router
