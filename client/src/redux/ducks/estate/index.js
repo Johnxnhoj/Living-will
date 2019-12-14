653b1cd936a17ce71030cfed41d0d76de0c9f77d
Denia Camacho <deniacamacho04@gmail.com>
2019-12-13 16:18:15 -0800

    ids

--- client/src/redux/ducks/estate/index.js
+++ client/src/redux/ducks/estate/index.js
@@ -1,20 +1,24 @@
 import { useSelector, useDispatch } from "react-redux"
+import { useEffect } from "react"
 import Axios from "axios"
 
 // action definitions
 
 const POST_ESTATE = "estate/POST_ESTATE"
+const GET_ESTATE = "estate/GET_ESTATE"
 
 // initial state
 const initialState = {
-  info: []
+  info: [],
+  estate: {}
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case POST_ESTATE:
       return { ...state, info: action.payload }
-
+    case GET_ESTATE:
+      return { ...state, info: action.payload }
     default:
       return state
   }
@@ -31,8 +35,22 @@ export function postToEstate(input) {
   }
 }
 
+export function getAssets(id) {
+  return dispatch => {
+    Axios.get("/estate/estate" + id).then(resp => {
+      console.log(resp.data[0])
+      dispatch({
+        type: GET_ESTATE,
+        payload: resp.data[0]
+      })
+    })
+  }
+}
 export function useEstate() {
   const dispatch = useDispatch()
+  const estate = useSelector(appState => appState.AssetsState.estate)
+  const getting = id => dispatch(getAssets(id))
   const grabEstateInfo = info => dispatch(postToEstate(info))
-  return { grabEstateInfo }
+  useEffect(() => {}, [dispatch])
+  return { grabEstateInfo, estate, getting }
 }
